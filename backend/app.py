67497c5cec37c976c46974f043ec623e6d24cc88
npadmanabhan05 <nitya.d.padmanabhan@gmail.com>
2025-01-30 15:54:11 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -231,7 +231,7 @@ def upload_file():
     return file_url, 200
 
 if __name__ == '__main__':
-    # app.run(host = '0.0.0.0', debug=True)
-    app.run(debug=True)
+    app.run(host = '0.0.0.0', debug=True)
+    #app.run(debug=True)
 
 
